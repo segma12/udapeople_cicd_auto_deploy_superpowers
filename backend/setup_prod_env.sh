7a6ca47cd2c717829b2f433aca24b7595b1c81e5
hishammagdy <hisham.magdy126@gmail.com>
2022-09-05 12:35:43 +0200

    setup prod env

--- backend/setup_prod_env.sh
+++ backend/setup_prod_env.sh
@@ -1,3 +1,3 @@
 export TYPEORM_MIGRATIONS_DIR=./migrations
-export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity.ts
 export TYPEORM_MIGRATIONS=./migrations/*.ts
